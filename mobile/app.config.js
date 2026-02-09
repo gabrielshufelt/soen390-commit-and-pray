55e72d295433f819e92dde31fa8ac4a6aa8711c7
Gabriel Shufelt <gabshu2014@gmail.com>
2026-02-09 17:33:52 -0500

    Configured directions and dotenv packages

---
+++ mobile/app.config.js
@@ -0,0 +1,70 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: "mobile",
+    slug: "mobile",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "mobile",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.anonymous.mobile"
+    },
+    android: {
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage: "./assets/images/android-icon-foreground.png",
+        backgroundImage: "./assets/images/android-icon-background.png",
+        monochromeImage: "./assets/images/android-icon-monochrome.png"
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      permissions: [
+        "android.permission.ACCESS_COARSE_LOCATION",
+        "android.permission.ACCESS_FINE_LOCATION"
+      ],
+      package: "com.anonymous.mobile"
+    },
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-location",
+        {
+          locationAlwaysAndWhenInUsePermission: "Allow $(PRODUCT_NAME) to use your location.",
+          locationWhenInUsePermission: "Allow $(PRODUCT_NAME) to use your location while you are using the app."
+        }
+      ],
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: {
+            backgroundColor: "#000000"
+          }
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true
+    },
+    extra: {
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+      router: {},
+      eas: {
+        projectId: "1eac18eb-8e25-4ee2-8a61-d0483ce49f82"
+      }
+    }
+  }
+};
