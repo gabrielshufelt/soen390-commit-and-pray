09166d63e0b8bbe7d41e5123e52d007bef88377c
Ahmad Saadawi <ahmad.saadawi7@gmail.com>
2026-03-21 18:27:26 -0400

    refactor: implement binary heap and remove coordinate-based path healing

--- mobile/utils/priorityQueue.ts
+++ mobile/utils/priorityQueue.ts
@@ -1,20 +1,47 @@
+type Entry<T> = { val: T; priority: number };
+
 export class PriorityQueue<T> {
-  private readonly values: { val: T; priority: number }[] = [];
+  private readonly heap: Entry<T>[] = [];
 
   enqueue(val: T, priority: number) {
-    this.values.push({ val, priority });
-    this.sort();
+    this.heap.push({ val, priority });
+    this.bubbleUp(this.heap.length - 1);
   }
 
   dequeue(): T | undefined {
-    return this.values.shift()?.val;
+    const top = this.heap[0];
+    const last = this.heap.pop()!;
+    if (this.heap.length > 0) {
+      this.heap[0] = last;
+      this.sinkDown(0);
+    }
+    return top?.val;
   }
 
   isEmpty() {
-    return this.values.length === 0;
+    return this.heap.length === 0;
   }
 
-  private sort() {
-    this.values.sort((a, b) => a.priority - b.priority);
+  private bubbleUp(i: number) {
+    while (i > 0) {
+      const parent = (i - 1) >> 1;
+      if (this.heap[parent].priority <= this.heap[i].priority) break;
+      [this.heap[parent], this.heap[i]] = [this.heap[i], this.heap[parent]];
+      i = parent;
+    }
+  }
+
+  private sinkDown(i: number) {
+    const n = this.heap.length;
+    while (true) {
+      const left = 2 * i + 1;
+      const right = 2 * i + 2;
+      let smallest = i;
+      if (left < n && this.heap[left].priority < this.heap[smallest].priority) smallest = left;
+      if (right < n && this.heap[right].priority < this.heap[smallest].priority) smallest = right;
+      if (smallest === i) break;
+      [this.heap[smallest], this.heap[i]] = [this.heap[i], this.heap[smallest]];
+      i = smallest;
+    }
   }
 }
